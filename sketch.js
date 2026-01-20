bc3a9893505d6e5daf0db1a87329ed02d599f8b0
n00241134 <N00241134@iadt.ie>
2026-01-20 12:21:18 +0000

    test

--- sketch.js
+++ sketch.js
@@ -1,3 +1,32 @@
+let names = ["John", "David", "Mary"];
+console.log(names);
+
+names[1] = "Steve";
+console.log(names);
+
+let score1 = 42;
+let score2 = 55;
+let score3 = 61;
+console.log(score1, score2, score3);
+
+let scores = [43, 56, 62];
+console.log(scores);
+
+let colours = ["red", "orange", "yellow"];
+console.log(colours[0]);
+
+names.push("Michael");
+console.log(names.length);
+
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.unshift("orange");
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
+
 function setup() {
 	createCanvas(500, 500);
 	background(55);
